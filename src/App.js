fbe030afdb3ee66a0e67a2fe2c9e6ce2737b1bcf
Henok Mekonnen <henatan99@yahoo.com>
2022-01-22 21:16:21 +0300

    Refactor hard code to dynamic component of Product and Rating

--- src/App.js
+++ src/App.js
@@ -1,144 +1,26 @@
 import React from 'react';
+import data from './data';
+import Product from './components/product';
 
 function App() {
   return (
     <div className="grid-container">
       <header className="row">
         <div>
-          <a href="index.html" className="brand">amazona</a>
+          <a href="/" className="brand">amazona</a>
         </div>
         <div>
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
         </div>
       </header>
       <main>
         <div className="row center">
-          <div className="card">
-            <a href="product.html">
-              <img className="medium" src="images/product-1.jpg" alt="product" />
-            </a>
-            <div className="card-body">
-              <a href="product.html">
-                <h2>Nike slim shirts</h2>
-              </a>
-              <div className="rating">
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-              </div>
-              <div className="price">
-                $120
-              </div>
-            </div>
-          </div>
-
-          <div className="card">
-            <a href="product.html">
-              <img className="medium" src="images/product-1.jpg" alt="product" />
-            </a>
-            <div className="card-body">
-              <a href="product.html">
-                <h2>Nike slim shirts</h2>
-              </a>
-              <div className="rating">
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-              </div>
-              <div className="price">
-                $120
-              </div>
-            </div>
-          </div>
-
-          <div className="card">
-            <a href="product.html">
-              <img className="medium" src="images/product-1.jpg" alt="product" />
-            </a>
-            <div className="card-body">
-              <a href="product.html">
-                <h2>Nike slim shirts</h2>
-              </a>
-              <div className="rating">
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-              </div>
-              <div className="price">
-                $120
-              </div>
-            </div>
-          </div>
-
-          <div className="card">
-            <a href="product.html">
-              <img className="medium" src="images/product-1.jpg" alt="product" />
-            </a>
-            <div className="card-body">
-              <a href="product.html">
-                <h2>Nike slim shirts</h2>
-              </a>
-              <div className="rating">
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-              </div>
-              <div className="price">
-                $120
-              </div>
-            </div>
-          </div>
-
-          <div className="card">
-            <a href="product.html">
-              <img className="medium" src="images/product-1.jpg" alt="product" />
-            </a>
-            <div className="card-body">
-              <a href="product.html">
-                <h2>Nike slim shirts</h2>
-              </a>
-              <div className="rating">
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-              </div>
-              <div className="price">
-                $120
-              </div>
-            </div>
-          </div>
-
-          <div className="card">
-            <a href="product.html">
-              <img className="medium" src="images/product-1.jpg" alt="product" />
-            </a>
-            <div className="card-body">
-              <a href="product.html">
-                <h2>Nike slim shirts</h2>
-              </a>
-              <div className="rating">
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star" /></span>
-                <span><i className="fa fa-star-half-o" /></span>
-                <span><i className="fa fa-star-o" /></span>
-              </div>
-              <div className="price">
-                $120
-              </div>
-            </div>
-          </div>
+          {
+            data.products.map((product) => (
+              <Product product={product} />
+            ))
+          }
         </div>
       </main>
       <footer className="row center">
---
+++ src/components/product.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Rating from './rating';
+
+const Product = ({ product }) => (
+  <div key={product._id} className="card">
+    <a href={`product/${product._id}`}>
+      <img className="medium" src={product.image} alt={product.name} />
+    </a>
+    <div className="card-body">
+      <a href="product.html">
+        <h2>{product.name}</h2>
+      </a>
+      <Rating rating={product.rating} numReviews={product.numReviews} />
+      <div className="price">{product.price}</div>
+    </div>
+  </div>
+);
+
+export default Product;
