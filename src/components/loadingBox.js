92aedcf6fcad27a0779cdc229463a1f71ff54843
Henok Mekonnen <henatan99@yahoo.com>
2022-01-24 23:29:42 +0300

    Add MessageBox and LoadingBox components and styling for them

---
+++ src/components/loadingBox.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LoadingBox = () => (
+    <div>
+        <i className="fa fa-spinner fa-spin" /> Loading...
+    </div>
+);
+
+export default LoadingBox;
--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -1,25 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Product from '../components/product';
+import LoadingBox from '../components/loadingBox';
+import MessageBox from '../components/messageBox';
 
 const HomeScreen = () => {
   const [products, setProducts] = useState([]);
-
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
-      const { data } = await axios.get('api/products');
-      setProducts(data);
+      try {
+        setLoading(true);
+        const { data } = await axios.get('api/products');
+        setLoading(false);
+        setProducts(data);
+      }
+      catch(err) {
+        setError(err.message);
+        setLoading(false);
+      }
     };
     fetchData();
   }, []);
 
   return (
-    <div className="row center">
-      {
+    <div>
+        {loading ? (
+          <LoadingBox />
+        ) : error ? (
+          <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
+          <div className="row center">
+            {
               products.map((product) => (
                 <Product key={product._id} product={product} />
               ))
             }
+          </div>
+        )}
     </div>
   );
 } 
