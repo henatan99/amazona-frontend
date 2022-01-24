c075379bbb6125c77e1d586a257b065fbaf606f8
Henok Mekonnen <henatan99@yahoo.com>
2022-01-24 22:46:35 +0300

    Add axios fetch AJAX request and add useState and useEffect hook in homeScreen

--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import data from '../data';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Product from '../components/product';
 
-const HomeScreen = () => (
-  <div className="row center">
-    {
-            data.products.map((product) => (
-              <Product key={product._id} product={product} />
-            ))
-          }
-  </div>
-);
+const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get('api/products');
+      setProducts(data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="row center">
+      {
+              products.map((product) => (
+                <Product key={product._id} product={product} />
+              ))
+            }
+    </div>
+  );
+} 
 
 export default HomeScreen;
